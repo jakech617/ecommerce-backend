4de69c6cb0a5416682ecf8577524ce63fd6aaaa0
Jake <jakech617@gmail.com>
2021-04-23 22:32:10 -0400

    fixed category put request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,10 +34,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const categoryData = await Category.create({
-      include: [{ model: Product }],
-      category_name: req.body.category_name
-    });
+    const categoryData = await Category.create(req.body); 
     
     res.status(200).json(categoryData);
   } catch (err) {
